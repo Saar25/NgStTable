72c3b5d5c80449b144fb06589108ab263c04db2e
Saar Tako <saartako25@gmail.com>
2022-06-03 17:51:12 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'st-table-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'StTableLibrary';
+}
